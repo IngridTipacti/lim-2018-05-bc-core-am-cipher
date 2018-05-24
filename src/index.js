b47f52098edb4c9778cc8f66010311b8f2b7a8aa
IngridTipacti <ingridtr23@gmail.com>
2018-05-24 18:26:33 -0500

    Estructura HTML básica, Avance Evento

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var btnCipher = document.getElementById("btnC");
+
+function result (){
+   var string=document.getElementById("writetextE").value;
+   alert(string);
+}
+
+btnCipher.addEventListener("click",result);
+
+
+
+
+// let btnEncrypted = document.getElementById("btnE");
+// let btnDecrypted = document.getElementById ("btnD");
+// let textForE = document.getElementById("writetextE");
+// let textForD = document.getElementById("writetextD")
+// let textR = document.getElementById("TxtResult")
+
+// document.getElementById("btnE").addEventListener("click",codificar);
+// function codificar (){
+//     var result= document.getElementById("writetextE").value;
+//     alert(resultados);
+//     }
